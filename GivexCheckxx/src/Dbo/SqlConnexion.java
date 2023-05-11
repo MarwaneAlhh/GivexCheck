56d19387d8b69bc4f580ddba0c4cc208517463e0
Al haddaoui Marwane <100091401+MarwaneAlh@users.noreply.github.com>
2023-05-11 21:05:50 +0200

    correct Build

---
+++ GivexCheckxx/src/Dbo/SqlConnexion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dbo;
+
+/**
+ *
+ * @author Marwa
+ */
+public class SqlConnexion {
+
+    private void SQLconnex() {
+        /*String a = "\"";
+        String connectionUrl = "jdbc:sqlserver://viper17" + a + "sql17";
+        try (
+                Connection connection = DriverManager.getConnection(connectionUrl);) {
+            System.out.println("Its working");
+        } // Handle any errors that may have occurred.
+        //Add everything 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }*/
+}
+}
---
+++ GivexCheckxx/src/main/GivexCheckxx.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author Marwa
+ */
+public class GivexCheckxx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
